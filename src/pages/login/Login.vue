5086f1d42b22bbc97caef180bea20a89ede8984e
pengtaoa <1564328778@qq.com>
2022-03-19 09:58:38 +0800

    feat: init

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useMainStore } from '@/store/main'
+import { ref } from 'vue'
+import { NButton } from 'naive-ui'
+
+const mainStore = useMainStore()
+
+// defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <h1>welcome to vue3</h1>
+    <div>{{ mainStore.name }}</div>
+    <n-button>naive-ui</n-button>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
